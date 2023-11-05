a7125d5961d8cbc34907366995fe4ee720e791e9
catcatcatau <31957687+catcatcatau@users.noreply.github.com>
2023-11-06 04:52:20 +1100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_forever():
+    for index in range(4):
+        basic.show_string("good night")
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function on_forever() {
+    for (let index = 0; index < 4; index++) {
+        basic.showString("good night")
+    }
+})
